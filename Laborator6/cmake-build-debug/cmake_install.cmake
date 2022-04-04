042b0b3082a47067b1c853d8e2965c198bdfba83
AlexNeagu123 <nalex4080@gmail.com>
2022-04-04 13:55:16 +0300

    Lab 6 added

--- Laborator6/cmake-build-debug/cmake_install.cmake
+++ Laborator6/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/alexandru/CLionProjects/Laborator6
+# Install script for directory: /home/alexandru/Documents/OOP/Laborator6
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/alexandru/CLionProjects/Laborator6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/alexandru/Documents/OOP/Laborator6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
