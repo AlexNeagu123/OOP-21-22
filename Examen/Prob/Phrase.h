5925e156232cc6d0ebd427777ec159a9286aaf25
AlexNeagu123 <nalex4080@gmail.com>
2022-04-26 13:41:31 +0300

    updates

--- Examen/Prob/Phrase.h
+++ Examen/Prob/Phrase.h
@@ -7,7 +7,20 @@
 
 
 class Phrase {
-
+    char ** Words;
+    int cntWords;
+    bool isVowel(char c);
+    int GetSize(char *word);
+    int Vowels(char *word);
+public:
+    Phrase(const char *sentence);
+    ~Phrase();
+    operator int() const;
+    char *operator[](int index);
+    int CountLetter(int index, char c);
+    int CountLetter(char c);
+    char* GetLongestWord();
+    int CountVowels();
 };
 
 
